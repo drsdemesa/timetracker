5fa0b975d829a21cdb94f5b72f5b359a0f2ec9fe
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2016-08-16 20:24:47 +0800

    display data on console

---
+++ scripts/controllers/TimeEntry.js
@@ -0,0 +1,26 @@
+(function() {
+    
+    'use strict';
+
+    angular
+        .module('timeTracker')
+        .controller('TimeEntry', TimeEntry);
+
+        function TimeEntry(time) {
+
+            // vm is our capture variable
+            var vm = this;
+
+            vm.timeentries = [];
+
+            // Fetches the time entries from the static JSON file
+            // and puts the results on the vm.timeentries array
+            time.getTime().then(function(results) {
+                vm.timeentries = results;
+                console.log(vm.timeentries);
+            }, function(error) { // Check for errors
+                console.log(error);
+            });
+
+        }
+})();
